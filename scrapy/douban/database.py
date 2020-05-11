8a02f63afa7bb153e0d6b0d77cc633ed9efc5b92
Eenki <eenki95@gmail.com>
2020-05-11 23:17:05 +0800

    2020-5-11update

--- scrapy/douban/database.py
+++ scrapy/douban/database.py
@@ -5,7 +5,8 @@ import pymysql
 # 定义连接数据库
 MYSQL_DB = 'douban'
 MYSQL_USER = 'root'
-MYSQL_PASS = 'HCthgu@mimi9'
+#MYSQL_PASS = 'HCthgu@mimi9'
+MYSQL_PASS = '123456'
 MYSQL_HOST = '127.0.0.1'
 connection = pymysql.connect(host=MYSQL_HOST, user=MYSQL_USER,
                              password=MYSQL_PASS, db=MYSQL_DB,
